944629057c01f34daf7e550572657295d91cc5f9
C4PT4INNULL <chaudharyarun5797@gmail.com>
2020-08-14 09:09:55 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import Header from "./components/layout/Header";
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
+
+export default App;
